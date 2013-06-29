891e687b7946113ce6b404799ac18dc1ff31f9bc
sergio <sbarrantes@yitec.net>
2013-06-29 10:05:29 -0600

    Primer Commit Carpetas machote

---
+++ machote.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link rel ="stylesheet" href="css/general_v2.css" type="text/css" />
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans+Condensed:300' rel='stylesheet' type='text/css' />
+        <title>XPD</title>
+    </head>
+    <body >
+    		<div class="header"></div>
+    		<div class="box">
+            <!--.Inserte Contenido aqui..-->
+			</div>		
+    </body>
+</html>
+
